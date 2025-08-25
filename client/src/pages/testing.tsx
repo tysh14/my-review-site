a65e79b9e5335f59c938a53e08d9dc8c0ef9e12e
Tyshka Visarra <visarrat@gmail.com>
2025-08-25 12:41:38 +0100

    removed testing page

--- client/src/pages/testing.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Testing = () => {
-  return (
-    <img src="https://www.gigwise.com/wp-content/uploads/2024/10/Major_2000_RB_Artists_and_Their_Contributions.jpeg" />
-  );
-};
-
-export default Testing;
